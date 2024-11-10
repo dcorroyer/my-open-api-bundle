6595f14e9546cbc95948eb3fe95823a6bcf70d2d
dcorroyer <dylan.corroyer@wyzgroup.com>
2024-11-10 21:18:31 +0100

    feat: clean features and security updates

--- src/Controller/BaseRestController.php
+++ src/Controller/BaseRestController.php
@@ -4,48 +4,47 @@ declare(strict_types=1);
 
 namespace My\RestBundle\Controller;
 
-use My\RestBundle\Enum\ApiResponseStatuses;
-use My\RestBundle\Enum\ErrorCodes;
-use My\RestBundle\Serialization\ApiSerializationGroups;
-use My\RestBundle\Utils\ObjectContextMerger;
-use Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination;
-use My\RestBundle\Serialization\Model\ApiResponse;
+use My\RestBundle\Dto\PaginatedResponseDto;
 use My\RestBundle\Serialization\Model\PaginatedListMetadata;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuilder;
 
 class BaseRestController extends AbstractController
 {
-    public function successResponse(
+    /**
+
+    @param array<string> $groups*/
+    protected function successResponse(
         mixed $data,
+        mixed $meta = null,
         array $groups = [],
         ObjectNormalizerContextBuilder $context = new ObjectNormalizerContextBuilder(),
-        array|object $meta = [],
-        int $status = Response::HTTP_OK,
-    ): JsonResponse {
-        $apiResponse = new ApiResponse(data: $data, meta: $meta);
+        int $status = Response::HTTP_OK
+    ): JsonResponse
+    {
+        $responseContext = $context->withGroups($groups);
 
-        $responseContext = $context
-            ->withGroups([...$groups, ApiSerializationGroups::API_SUCCESS]);
+        $response = [
+            'data' => $data,
+        ];
 
-        return $this->json(
-            data: $apiResponse,
-            status: $status,
-            context: ObjectContextMerger::mergeContext($context, $responseContext)->toArray()
-        );
+        if ($meta !== null) {
+            $response['meta'] = $meta;
+        }
+
+        return $this->json(data: $response, status: $status, context: $responseContext->toArray());
     }
 
-    public function paginateResponse(SlidingPagination $pagination, array $groups): JsonResponse
+    public function paginatedResponse(PaginatedResponseDto $pagination): JsonResponse
     {
         /** @var array<object> $data */
-        $data = $pagination->getItems();
+        $data = $pagination->data;
 
-        $page = $pagination->getCurrentPageNumber();
-        $itemsPerPage = $pagination->getItemNumberPerPage();
-        $total = $pagination->getTotalItemCount();
+        $page = $pagination->meta->page;
+        $itemsPerPage = $pagination->meta->limit;
+        $total = $pagination->meta->total;
         $firstItem = count($data) > 0 ? ($page - 1) * $itemsPerPage + 1 : 0;
         $lastItem = count($data) > 0 ? $firstItem + count($data) - 1 : 0;
         $hasMore = $itemsPerPage !== -1 && $total > ($itemsPerPage * $page);
@@ -58,56 +57,6 @@ class BaseRestController extends AbstractController
             ->setTotal($total)
             ->setHasMore($hasMore);
 
-        return $this->successResponse(data: $data, groups: $groups, meta: $meta);
-    }
-
-    protected function errorResponse(
-        string $message = '',
-        array $errors = [],
-        int $responseCode = Response::HTTP_BAD_REQUEST,
-        ObjectNormalizerContextBuilder $context = new ObjectNormalizerContextBuilder(),
-        ?string $code = null,
-    ): JsonResponse {
-        $apiResponse = new ApiResponse(
-            data: null,
-            errors: $errors,
-            message: $message,
-            status: ApiResponseStatuses::STATUS_ERROR
-        );
-
-        $defaultGroups = [ApiSerializationGroups::API_ERROR];
-        if ($code !== null) {
-            $apiResponse->setCode($code);
-            $defaultGroups[] = ApiSerializationGroups::API_ERROR_CODE;
-        }
-
-        $responseContext = $context
-            ->withGroups([...$defaultGroups]);
-
-        return $this->json(
-            data: $apiResponse,
-            status: $responseCode,
-            context: (array) ObjectContextMerger::mergeContext($context, $responseContext)
-        );
-    }
-
-    protected function notFoundResponse(
-        bool $thrown = false
-    ): JsonResponse {
-        $message = ErrorCodes::NOT_FOUND->value;
-        if ($thrown) {
-            throw new NotFoundHttpException($message);
-        }
-        return $this->errorResponse(message: $message, errors: [ErrorCodes::NOT_FOUND], responseCode: Response::HTTP_NOT_FOUND);
-    }
-
-    protected function createNoContentResponse(): Response
-    {
-        return new Response(null, Response::HTTP_NO_CONTENT);
-    }
-
-    protected function createUnprocessableResponse(?string $message = null): Response
-    {
-        return new Response($message ?? 'Unprocessable error', Response::HTTP_UNPROCESSABLE_ENTITY);
+        return $this->successResponse(data: $data, meta: $meta);
     }
 }
